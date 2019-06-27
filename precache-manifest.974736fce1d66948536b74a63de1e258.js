445838172f13b20bdb269fe6618a99bd62ef8509
Renaud Tertrais <renaud.tertrais@gmail.com>
2019-06-27 23:24:05 +0200

    Updates

---
+++ precache-manifest.974736fce1d66948536b74a63de1e258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6685749ca2ba0ef362d3302440a598a1",
+    "url": "/animals/index.html"
+  },
+  {
+    "revision": "3f7f0cd94e234deb738f",
+    "url": "/animals/static/css/2.39bbabe8.chunk.css"
+  },
+  {
+    "revision": "27d51353ad5588b58a79",
+    "url": "/animals/static/css/main.93ba529f.chunk.css"
+  },
+  {
+    "revision": "3f7f0cd94e234deb738f",
+    "url": "/animals/static/js/2.84248cfa.chunk.js"
+  },
+  {
+    "revision": "27d51353ad5588b58a79",
+    "url": "/animals/static/js/main.700fca9a.chunk.js"
+  },
+  {
+    "revision": "73c2e48670228928808f",
+    "url": "/animals/static/js/runtime~main.5da64527.js"
+  }
+]);
